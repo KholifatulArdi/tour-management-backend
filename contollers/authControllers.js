ada583e06f63e864fbc5bb4f14144a88b9ff4002
Kholifatul Ardi <2tkj2.kholifatul@gmail.com>
2023-11-19 21:18:09 +0700

    initial commit

---
+++ contollers/authControllers.js
@@ -0,0 +1,54 @@
+import User from "../models/User.js";
+import bcrypt from 'bcryptjs'
+import jwt from "jsonwebtoken"
+
+export const register = async(req,res) => {
+    try {
+
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(req.body.password, salt)
+
+        const newUser = new User({
+            username: req.body.username,
+            email:req.body.email,
+            password:hash,
+            photo:req.body.photo
+        })
+        
+        await newUser.save();
+
+        res.status(200).json({success:true, msg:"Successfully Created"})
+    } catch (error) {
+        res.status(500).json({success:false, msg:"Failed To Created"})
+    }
+}
+
+export const login = async(req,res) => {
+    const email = req.body.email
+    try {
+        const user = await User.findOne({email})
+        
+        if(!user){
+            return res.status(404).json({success:false, msg:"User Not Found"})
+        }
+        const checkCorrectPassword = await bcrypt.compare(req.body.password, user.password)
+
+        //cek password
+        if(!checkCorrectPassword){
+            return res.status(401).json({success:false, msg:"incorect email or password"})
+        }
+
+        const {password, role, ...rest} = user._doc
+
+        //create jwt token
+        const token = jwt.sign({id:user._id, role:user.role}, process.env.JWT_SECRET_KEY, {expiresIn: "15d"})
+
+        //set token in the browser cookies and send the response to the client
+        res.cookie('accessToken', token,{
+            httpOnly: true,
+            expires:token.expiresIn
+        }).status(200).json({token, data: {...rest,role}})
+    } catch (error) {
+        res.status(500).json({success:false, msg:"Failed To Login"})
+    }
+}
